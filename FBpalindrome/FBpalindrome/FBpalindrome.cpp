a4b9b500d40c1181480ff82dc7a8759d819a67cd
fbeaman <47163792+fbeaman@users.noreply.github.com>
2019-04-04 01:26:33 -0500

    Add files via upload

---
+++ FBpalindrome/FBpalindrome/FBpalindrome.cpp
@@ -0,0 +1,59 @@
+/*
+Foster Beaman
+
+Norhtwest Vista
+
+20190404 v. 1.00
+
+This program is to check whether a user input string is a palindrome or not.
+*/
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+Psuedo Code
+	1. Output set to desired look.
+	2. Boolean to check if palindrome.
+	3. User input for palindrome.
+	4. Do While loop for multiple iterations.
+*/
+
+//Boolean to check if palindrome.
+bool isPalindrome(string strPalin)
+{
+	int intLength = str.length(strPalin);
+
+	//For look to check if palindrome.
+	for (int intI = 0; intI < intLength / 2; intI++)
+		if (strPalin != str[intLength - 1 - intI])
+			
+			return false;
+
+	return true;
+}
+int main()
+{
+	//String Variable
+	string strPalin = " ";
+
+	//Output
+	cout << "Please enter a string to check if it is a palindrome." << endl;
+	cout << "Enter # to quit." << endl;
+	
+	//Do While for multiple palindrome checks.
+	do {
+		cout << "Entry: ";
+		
+		//Input
+		cin >> strPalin;
+		cout << endl;
+
+		isPalindrome(strPalin);
+
+	} while (strPalin != "#");
+
+	return 0;
+}
